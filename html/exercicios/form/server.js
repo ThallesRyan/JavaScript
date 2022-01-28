01a41d218227e059324436cb77b073ceb83343c7
ThallesRyan <thallesryan2@gmail.com>
2022-01-28 16:31:08 -0300

    Formulário

--- html/exercicios/form/server.js
+++ html/exercicios/form/server.js
@@ -3,10 +3,16 @@ const app = express()
 const bodyParser = require('body-parser')
 
 app.use(bodyParser.urlencoded({extended:true}))
-
+//Para postar um novo usuário
 app.post('/users',(req, resp)=>{
     console.log(req.body)
-    res.send( '<h1>Parabéns</h1>')
+    resp.send( '<h1>Parabéns</h1>')
+})
+//Para alterar um usuário já cadastrado
+app.post('/users/:id', (req, resp) => {
+    console.log(req.params.id)
+    console.log(req.body)
+    resp.send('<h1>Parabéns. Usuário Alterado!!!</h1>')
 })
 
 app.listen(3034)
